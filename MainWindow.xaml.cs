7c3364573ed2b9ba738213bb45f860890902b6d3
michw85 <47576594+michw85@users.noreply.github.com>
2020-11-24 10:08:04 +0200

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyViber
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        bool IsToggle;
+        private void Btn_Click(object sender, RoutedEventArgs e)
+        {
+            DoubleAnimation da = new DoubleAnimation();
+            if (!IsToggle)
+            {
+                da.To = 90;
+                da.Duration = TimeSpan.FromSeconds(1);
+                brd.BeginAnimation(Border.HeightProperty, da);
+                IsToggle = true;
+            }
+            else
+            {
+                da.To = 0;
+                da.Duration = TimeSpan.FromSeconds(1);
+                brd.BeginAnimation(Border.HeightProperty, da);
+                IsToggle = false;
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            brd.Height = 0;
+
+        }
+    }
+}
---
+++ MainWindowViewModel.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.Windows;
+
+using MyViber.Base;
+using MyViber.ChatContacts;
+using MyViber.ChatLog;
+using MyViber.ChatBot;
+
+namespace MyViber
+{
+    class MainWindowViewModel : Base.ViewModel
+    {
+
+        public ObservableCollection<ChatContactsNode> Contacts { get; set; }
+        public ObservableCollection<ChatLogNode> Chat { get; set; }
+        public string align { get; set; }
+        string _nikName;
+        public string userMsg { get; set; }
+
+        public ChatContactsNode nikName
+        {
+            set
+            {
+                _nikName = value.nikName;
+                if (value != null)
+                {
+                    MessageBox.Show(_nikName);
+                    Chat = ChatLogModel.getInstance().getChat(_nikName);
+                    OnPropertyChanged(nameof(Chat));
+                }
+            }
+        }
+
+
+
+       
+        public Command SendMsgToChat { get; set; }
+
+        private void setupCommands()
+        {
+          
+            SendMsgToChat = new Command(() =>
+            {
+                if (this._nikName == null)
+                {
+                    MessageBox.Show("Select User to send " + userMsg);
+                    return;
+                }
+               
+                ChatLogModel.getInstance().addMsgToChat(_nikName, "You", userMsg);
+
+ 
+
+                string[] words = userMsg.Split();
+                foreach (string word in words)
+                {
+                   
+                    ObservableCollection<string> Answers = ChatBot.ChatBotModel.getInstance().getAnswers(word);
+                    if (Answers.Count < 1)
+                    {
+                        ChatLogModel.getInstance().addMsgToChat(_nikName, _nikName, "Ура!!!");
+                    }
+                    else
+                    {
+                        ChatLogModel.getInstance().addMsgToChat(_nikName, _nikName, Answers[1]);
+                    }
+
+
+
+                }
+
+                Chat = ChatLogModel.getInstance().getChat(_nikName);
+                OnPropertyChanged(nameof(Chat));
+
+                userMsg = "";
+                OnPropertyChanged(nameof(userMsg));
+
+            });
+        }
+
+        // Конструктор
+        public MainWindowViewModel()
+        {
+            Contacts = ChatContactsModel.getInstance().getContacts();
+            setupCommands();
+ 
+        }
+
+    }
+}
